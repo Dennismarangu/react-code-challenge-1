a9984bd41269d22d20d9a9d5a54e6956f34c058b
Dennismarangu <dennismutumamarangu@gmail.com>
2023-04-24 09:45:25 +0300

    Initial commit

--- bank-of-flatiron-app/src/App.js
+++ bank-of-flatiron-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import Home from './components/Home';
+import TransactionTable from './components/TransactionTable';
 import About from './components/About';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div>
      <NavBar />
      <main className="main-content"></main>
-     <Home />
+     <TransactionTable />
      <About />
     </div> 
   );
---
+++ bank-of-flatiron-app/src/components/TransactionTable.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import "./TransactionTable.css";
+
+function TransactionTable() {
+  const [transactions, setTransactions] = useState([]);
+  const [newTransaction, setNewTransaction] = useState({ date: '', description: '', category: '', amount: '' });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/db.json');
+        const data = await response.json();
+        setTransactions(data.transactions);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setTransactions(transactions => [...transactions, newTransaction]);
+    setNewTransaction({ date: '', description: '', category: '', amount: '' });
+  };
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setNewTransaction({ ...newTransaction, [name]: value });
+  };
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const filteredTransactions = transactions.filter(transaction => transaction.description.toLowerCase().includes(filter.toLowerCase()));
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" name="date" value={newTransaction.date} onChange={handleChange} />
+        <label htmlFor="description">Description:</label>
+        <input type="text" id="description" name="description" value={newTransaction.description} onChange={handleChange} />
+        <label htmlFor="category">Category:</label>
+        <select id="category" name="category" value={newTransaction.category} onChange={handleChange}>
+          <option value="Income">Income</option>
+          <option value="Food">Food</option>
+          <option value="Fashion">Fashion</option>
+          <option value="Gift">Gift</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Housing">Housing</option>
+        </select>
+        <label htmlFor="amount">Amount:</label>
+        <input type="number" id="amount" name="amount" value={newTransaction.amount} onChange={handleChange} />
+        <button type="submit">Add Transaction</button>
+      </form>
+      <br />
+      <label htmlFor="filter">Filter by Description:</label>
+      <input type="text" id="filter" name="filter" value={filter} onChange={handleFilterChange} />
+      <br />
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredTransactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TransactionTable;
